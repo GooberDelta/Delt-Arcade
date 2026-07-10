98f99d330ef6144ae6b26e6ec6a328707ee01b84
Delta <93168002+GooberDelta@users.noreply.github.com>
2026-07-10 18:18:05 -0400

    Update login.js

--- WebUI/js/login.js
+++ WebUI/js/login.js
@@ -1,1 +1,6 @@
-
+function login() {
+    // This has not been coded yet, so just display an error while attempting to login.
+    let errmsg = document.getElementById("error_message");
+    errmsg.style.display = "block";
+    errmsg.textContent = "404: Login has not been coded"
+}
