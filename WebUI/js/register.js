da82f484a91548c763ce9379ff74061e85636c96
GooberDelta <93168002+GooberDelta@users.noreply.github.com>
2026-07-11 12:33:39 -0400

    Automatic Safety Backup

--- WebUI/js/register.js
+++ WebUI/js/register.js
@@ -1,1 +1,16 @@
+function register() {
+	// Form Areas.
+	let username = document.getElementById("username");
+	let displayName = document.getElementById("dn");
+	let pw = document.getElementById("pw");
+	let rtpw = document.getElementById("rtpw");
+	//Error messages.
 
+	// Let's make sure the passwords are the same.
+	if pw.value() == rtpw.value() {
+		console.log("Passwords were verified.")
+	}
+	else {
+		console.error("Passwords did not match.")
+	}
+}
