18aef66f13263a9afc64cb8137cd4eb04c8c0757
GooberDelta <93168002+GooberDelta@users.noreply.github.com>
2026-07-11 13:04:45 -0400

    Automatic Safety Backup

--- WebUI/js/register.js
+++ WebUI/js/register.js
@@ -1,16 +1,28 @@
 function register() {
 	// Form Areas.
-	let username = document.getElementById("username");
-	let displayName = document.getElementById("dn");
-	let pw = document.getElementById("pw");
-	let rtpw = document.getElementById("rtpw");
+	let username = document.getElementById("username").value;
+	let displayName = document.getElementById("dn").value;
+	let email = document.getElementById("email").value;
+	let pw = document.getElementById("pw").value;
+	let rtpw = document.getElementById("rtpw").value;
 	//Error messages.
-
-	// Let's make sure the passwords are the same.
-	if (pw.value() == rtpw.value()) {
-		console.log("Passwords were verified.")
+	let errmsg = document.getElementById("error_message");
+	// Checking for email
+	if (email.includes("@")) {
+		console.log("email is a real email.")
 	}
 	else {
-		console.error("Passwords did not match.")
+		console.error("Email is not real.")
+		errmsg.style.display = "block";
+		errmsg.textContent = "Please enter a valid email."
+	}
+	// Let's make sure the passwords are the same.
+	if (pw.value == rtpw) {
+		console.log("Passwords were verified.");
+	}
+	else {
+		console.error("Passwords did not match.");
+		errmsg.style.display = "block";
+		errmsg.textContent = "The passwords do not match.";
 	}
 }
