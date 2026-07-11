3d293a11c011e2fb9e649530f8d624844909af31
GooberDelta <93168002+GooberDelta@users.noreply.github.com>
2026-07-11 13:26:47 -0400

    Added Registration check JS Code

--- WebUI/js/register.js
+++ WebUI/js/register.js
@@ -1,42 +1,44 @@
 function register() {
 	// Form Areas.
-	let username = document.getElementById("username").value;
-	let displayName = document.getElementById("dn").value;
-	let email = document.getElementById("email").value;
-	let pw = document.getElementById("pw").value;
-	let rtpw = document.getElementById("rtpw").value;
+	let username = document.getElementById("username");
+	let displayName = document.getElementById("dn");
+	let email = document.getElementById("email");
+	let pw = document.getElementById("pw");
+	let rtpw = document.getElementById("rtpw");
 	//Error message.
 	let errmsg = document.getElementById("error_message");
 	
 
 	// Checking for any non-filled out info.
-	if (username = "") {
+	if (username.value == "") {
 		console.error("Username isn't filled out.");
 		errmsg.style.display = "block";
 		errmsg.textContent = "A username is required.";
 	}
-	else if (email = "") {
+	else if (email.value == "") {
 		console.error("Email isn't filled out.");
 		errmsg.style.display = "block";
 		errmsg.textContent = "An Email is required.";
 	}
-	else if ()
 	// Checking for email
-	if (email.includes("@")) {
-		console.log("email is a real email.")
-	}
-	else {
+	else if (email.value.includes("@") == false) {
 		console.error("Email is not real.")
 		errmsg.style.display = "block";
 		errmsg.textContent = "Please enter a valid email."
 	}
-	// Let's make sure the passwords are the same.
-	if (pw.value == rtpw) {
-		console.log("Passwords were verified.");
+	else if (pw.value == "") {
+		console.error("No password entered.")
+		errmsg.style.display = "block";
+		errmsg.textContent = "A password is required.";
 	}
-	else {
+	// Let's make sure the passwords are the same.
+	else if (pw.value != rtpw.value) {
 		console.error("Passwords did not match.");
 		errmsg.style.display = "block";
 		errmsg.textContent = "The passwords do not match.";
+	}
+	else {
+		console.log("Registration test passed!")
+		errmsg.style.display = "none";
 	}
 }
