083fc2c7768ee8408adf24f773dc1d205defb7c4
GooberDelta <93168002+GooberDelta@users.noreply.github.com>
2026-07-12 13:27:38 -0400

    Code frmo 7/12/26 1:27 PM EST

--- WebUI/js/login.js
+++ WebUI/js/login.js
@@ -1,10 +1,10 @@
 let uri = "http://127.0.0.1:8000"
 
-function login() {
+async function login() {
     let errmsg = document.getElementById("error_message");
     let userbody = document.getElementById("un");
     let passbody = document.getElementById("pw");
-    const response = fetch(uri + "/auth/login", {
+    const response = await fetch(uri + "/auth/login", {
       method: "POST",
       body: JSON.stringify({
         username:userbody.value, 
@@ -14,8 +14,8 @@ function login() {
         "Content-type": "application/json; charset=UTF-8"
       }
     })
-    console.log(response.status);
-    if (response.status == "200 OK") {
+    console.log("Status Code: " + response.status);
+    if (response.status = "") {
       window.location("/dashboard/overview")
     }
     else if (response.status == "401 Unauthorized") {
@@ -24,8 +24,13 @@ function login() {
       console.error("The server responded with 401.")
     }
     else {
+      const data = response
+      .then((result) => {
+        console.log(data)
+      })
       errmsg.style.display = "block";
-      errmsg.textContent = "The server cannot be reached, please check the console for more information."
+      console.log("The data is: " + data)
+      errmsg.textContent = data
       console.error("Couldn't Connect to server.")
     }
 }
